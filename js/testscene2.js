d4b18e57c5b2324147a553948c8e4cc8f9d8851b
Ryan Cashman <ryan.j.cashman@gmail.com>
2018-07-17 22:23:34 -0700

    randomized colors and trackball controls, also linking to three.js lib through web

--- js/testscene2.js
+++ js/testscene2.js
@@ -3,7 +3,9 @@ var geometry, material, line;
 var x, y, z;
 
 var camMovement = 0;
-var pointCount = 50000;
+var pointCount = 10000;
+
+var controls;
 //
 // var ljSettings = {
 //   freq : new THREE.Vector3( 12, 22, 12 ),
@@ -20,6 +22,7 @@ function init()
   renderer = new THREE.WebGLRenderer( { antialias : true } );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
+  controls = new THREE.TrackballControls(camera, renderer.domElememnt);
 }
 
 function start (){
@@ -38,15 +41,16 @@ function start (){
     //geometry.setDrawRange(0, pointCount-1); // might not be needed if drawing all?
     line = new THREE.Line( geometry, material );
 
-    setColors();
+    setRandomColors();
+
     geometry.computeBoundingSphere();
 
     scene.add( line );
     camera.position.z = 5;
-    camera.lookAt(new THREE.Vector3());
+    camera.lookAt( new THREE.Vector3() );
 }
 
-function setColors()
+function setRandomColors()
 {
   var colors = line.geometry.attributes.color.array;
   var u = v = w = 0.1;
@@ -64,7 +68,7 @@ function setColors()
   line.geometry.attributes.color.needsUpdate = true; // VERY IMPORTANT!!
 }
 
-function updatePositions()
+function randomPositions()
 {
   var positions = line.geometry.attributes.position.array;
 
@@ -74,19 +78,42 @@ function updatePositions()
     positions[ index ++ ] = y;
     positions[ index ++ ] = z;
 
-    x = Math.random() * (Math.random() > 0.5 ? 2 : -2);
-    y = Math.random() * (Math.random() > 0.5 ? 2 : -2);
-    z = Math.random() * (Math.random() > 0.5 ? 2 : -2);
+    x = Math.random() * ( Math.random() > 0.5 ? 2 : -2 );
+    y = Math.random() * ( Math.random() > 0.5 ? 2 : -2 );
+    z = Math.random() * ( Math.random() > 0.5 ? 2 : -2 );
   }
 
   line.geometry.attributes.position.needsUpdate = true; // VERY IMPORTANT!!
 }
 
+function setLissajousPositions(phi)
+{
+  var positions = line.geometry.attributes.position.array;
+
+  var index = 0;
+  for (var i = 0; i < pointCount; i ++ ) {
+    var _angle = THREE.Math.mapLinear( i, 0, pointCount, 0, Math.PI * 2 );
+
+    positions[ index ++ ] = computeLissajousPosition(_angle, 22, 8, phi, 2);
+    positions[ index ++ ] = computeLissajousPosition(_angle, 14, 5, phi, 2);
+    positions[ index ++ ] = computeLissajousPosition(_angle, 16, 4, phi, 2);
+
+  }
+  line.geometry.attributes.position.needsUpdate = true; // VERY IMPORTANT!!
+}
+
+function computeLissajousPosition(angle, freq, mod, phi, scale)
+{
+  return (Math.sin( angle * freq + THREE.Math.degToRad( phi ) ) * Math.cos(angle * mod ) ) * scale;
+}
+
 function animate() {
     requestAnimationFrame( animate );
     //camMovement+=0.01;
-    updatePositions();
-
+    // randomPositions();
+    var time = Date.now() * 0.1;
+    setLissajousPositions(time);
+    controls.update()
 //    camera.position.x = Math.sin(camMovement)*5;
     // camera.lookAt(new THREE.Vector3());
     renderer.render( scene, camera );
