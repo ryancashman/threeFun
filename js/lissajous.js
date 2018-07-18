49fb258b36e8bac95fd0472dc0d1ef26f0cf9ba7
Ryan Cashman <ryan@nanoanimal.com>
2018-07-18 11:23:53 -0700

    moved lissajous stuff to lissajous.js, working

--- js/lissajous.js
+++ js/lissajous.js
@@ -1,21 +1,20 @@
-// 
-// function setLissajousPositions(line, phi)
-// {
-//   var positions = line.geometry.attributes.position.array;
-//
-//   var index = 0;
-//   for (var i = 0; i < pointCount; i ++ ) {
-//     var _angle = THREE.Math.mapLinear( i, 0, pointCount, 0, Math.PI * 2 );
-//
-//     positions[ index ++ ] = computeLissajousPosition(_angle, 15, 2, phi, 2);
-//     positions[ index ++ ] = computeLissajousPosition(_angle, 13, 3, phi*0.4, 2);
-//     positions[ index ++ ] = computeLissajousPosition(_angle, 12, 5, phi*0.2, 2);
-//
-//   }
-//   line.geometry.attributes.position.needsUpdate = true; // VERY IMPORTANT!!
-// }
-//
-// function computeLissajousPosition(angle, freq, mod, phi, scale)
-// {
-//   return (Math.sin( angle * freq + THREE.Math.degToRad( phi ) ) * Math.cos(angle * mod ) ) * scale;
-// }
+function setLissajousPositions(line, phi)
+{
+  var positions = line.geometry.attributes.position.array;
+
+  var index = 0;
+  for (var i = 0; i < pointCount; i ++ ) {
+    var _angle = THREE.Math.mapLinear( i, 0, pointCount, 0, Math.PI * 2 );
+
+    positions[ index ++ ] = computeLissajousPosition(_angle, 15, 4, phi, 2);
+    positions[ index ++ ] = computeLissajousPosition(_angle, 14, 5, phi*0.5, 2);
+    positions[ index ++ ] = computeLissajousPosition(_angle, 11, 2, phi*0.2, 2);
+
+  }
+  line.geometry.attributes.position.needsUpdate = true; // VERY IMPORTANT!!
+}
+
+function computeLissajousPosition(angle, freq, mod, phi, scale)
+{
+  return (Math.sin( angle * freq + THREE.Math.degToRad( phi ) ) * Math.cos(angle * mod ) ) * scale;
+}
--- js/testscene2.js
+++ js/testscene2.js
@@ -11,7 +11,8 @@ function init()
 {
   x = y = z = 0;
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 75, window.devicePixelRatio, 0.1, 100 );
+  // camera = new THREE.PerspectiveCamera( 75, window.devicePixelRatio, 0.1, 100 );
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
   renderer = new THREE.WebGLRenderer( { antialias : true } );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
@@ -52,27 +53,6 @@ function setRandomColors(line)
   }
 
   line.geometry.attributes.color.needsUpdate = true; // VERY IMPORTANT!!
-}
-
-function setLissajousPositions(line, phi)
-{
-  var positions = line.geometry.attributes.position.array;
-
-  var index = 0;
-  for (var i = 0; i < pointCount; i ++ ) {
-    var _angle = THREE.Math.mapLinear( i, 0, pointCount, 0, Math.PI * 2 );
-
-    positions[ index ++ ] = computeLissajousPosition(_angle, 15, 4, phi, 2);
-    positions[ index ++ ] = computeLissajousPosition(_angle, 14, 5, phi*0.5, 2);
-    positions[ index ++ ] = computeLissajousPosition(_angle, 11, 2, phi*0.2, 2);
-
-  }
-  line.geometry.attributes.position.needsUpdate = true; // VERY IMPORTANT!!
-}
-
-function computeLissajousPosition(angle, freq, mod, phi, scale)
-{
-  return (Math.sin( angle * freq + THREE.Math.degToRad( phi ) ) * Math.cos(angle * mod ) ) * scale;
 }
 
 function animate() {
