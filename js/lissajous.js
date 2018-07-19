3b0a30a2fab0d5db6b2deec7becf5bfd694112e3
Ryan Cashman <ryan.j.cashman@gmail.com>
2018-07-18 22:07:34 -0700

    using const whenever possible, dat-gui overhaul

--- js/lissajous.js
+++ js/lissajous.js
@@ -1,14 +1,14 @@
-function setLissajousPositions(line, phi, settings, closed)
+function setLissajousPositions(line, params, maxPoints, closed)
 {
-  var positions = line.geometry.attributes.position.array;
+  const _positions = line.geometry.attributes.position.array;
 
-  var index = 0;
-  for (var i = 0; i < pointCount; i ++ ) {
-    var _angle = THREE.Math.mapLinear( ( (closed && i == pointCount - 1) ? 0 : i ) , 0, pointCount, 0, Math.PI * 2 );
-
-    positions[ index ++ ] = computeLissajousPosition(_angle, settings.freq.x, settings.mod.x, phi, settings.scale);
-    positions[ index ++ ] = computeLissajousPosition(_angle, settings.freq.y, settings.mod.y, phi*0.5, settings.scale);
-    positions[ index ++ ] = computeLissajousPosition(_angle, settings.freq.z, settings.mod.z, phi*0.2, settings.scale);
+  var _index = 0;
+  for (var i = 0; i < maxPoints; i ++ ) {
+    var _angle = THREE.Math.mapLinear( ( (closed && i == maxPoints - 1) ? 0 : i ) , 0, maxPoints, 0, Math.PI * 2 );
+//Could be a loop
+    _positions[ _index ++ ] = computeLissajousPosition( _angle, params.freq[0], params.mod[0], params.offset * params.speed[0], params.scale[0] );
+    _positions[ _index ++ ] = computeLissajousPosition( _angle, params.freq[1], params.mod[1], params.offset * params.speed[1], params.scale[1] );
+    _positions[ _index ++ ] = computeLissajousPosition( _angle, params.freq[2], params.mod[2], params.offset * params.speed[2], params.scale[2] );
 
   }
   line.geometry.attributes.position.needsUpdate = true; // VERY IMPORTANT!!
@@ -17,4 +17,16 @@ function setLissajousPositions(line, phi, settings, closed)
 function computeLissajousPosition(angle, freq, mod, phi, scale)
 {
   return (Math.sin( angle * freq + THREE.Math.degToRad( phi ) ) * Math.cos(angle * mod ) ) * scale;
+}
+
+function setRandomColors(line, maxPoints)
+{
+  const colors = line.geometry.attributes.color.array;
+  var index = 0;
+  for (var i = 0; i < maxPoints; i ++ ) {
+    colors[ index ++ ] = Math.random();
+    colors[ index ++ ] = Math.random();
+    colors[ index ++ ] = Math.random();
+  }
+  line.geometry.attributes.color.needsUpdate = true; // VERY IMPORTANT!!
 }
--- js/ljtestscene.js
+++ js/ljtestscene.js
@@ -2,37 +2,43 @@
 // const gui = new dat.GUI();
 // const THREE = require('three');
 
-var scene, camera, renderer;
+//THIS CouLD ALL BE WRAPPED UP INTO A MODULE
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
+const renderer = new THREE.WebGLRenderer( { antialias : true } );
+// var scene, camera, renderer;
 var geometry, material, ljLine;
-var x, y, z;
+// var x, y, z;
 
-var camMovement = 0;
-var pointCount = 1200;
+const camMovement = 0;
+const pointCount = 1200;
 
-var controls;
-var gui;
+// var controls;
+// var gui;
+const gui = new dat.GUI( {
+  height : 5 * 32 - 1 } );
 
-var params = function()
+const ljParams = function()
 {
-  this.message = "yo yo yo!";
-  this.a = 12;
-  this.b = 42;
-};
-
-var ljSettings = {
-  freq : new THREE.Vector3( 5, 6, 3 ),
-  mod : new THREE.Vector3 ( 2, 2, 1 ),
-  scale : 3
+  this.freq = [ 4, 8, 2 ],
+  this.mod = [ 1, 2, 3 ],
+  this.scale = [ 1, 1, 1 ],
+  this.speed = [ 1, 1, 1 ],
+  this.offset = 0
 }
+
+var ljp;
 
 function initScene()
 {
   //alert("Hi!");
-  x = y = z = 0;
-  scene = new THREE.Scene();
-  // camera = new THREE.PerspectiveCamera( 75, window.devicePixelRatio, 0.1, 100 );
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
-  renderer = new THREE.WebGLRenderer( { antialias : true } );
+  // x = y = z = 0;
+  // scene = new THREE.Scene();
+  // // camera = new THREE.PerspectiveCamera( 75, window.devicePixelRatio, 0.1, 100 );
+  // camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
+  // renderer = new THREE.WebGLRenderer( { antialias : true } );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
@@ -41,43 +47,53 @@ function initScene()
 }
 
 function initGUI(){
-  gui = new dat.GUI( {
-    height : 5 * 32 - 1 } );
+  // gui = new dat.GUI( {
+  //   height : 5 * 32 - 1 } );
 
-  gp = new params();
+  ljp = new ljParams(); // for whatever reason this needs to be a new object.
 
-  var a = gui.add(gp, 'a', 1, 100).name("Parm A");
-  a.onChange(function (value){
-    //console.log("changed! " + value.toString());
-    ljSettings.freq.x = value;
+  const freq = gui.addFolder("Freq");
+  Object.keys(ljp.freq).forEach((key) => {
+    freq.add(ljp.freq, key, 0, 100);
   });
+  freq.open();
 
-  var b = gui.add(gp, 'b', 1, 100).name("Parm B");
-  b.onChange(function (value){
-    ljSettings.freq.y = value;
-  })
-  gui.add(gp, 'message', 1, 10).name("Message");
-  //gui.open();
+  const mod = gui.addFolder("Mod");
+  Object.keys(ljp.mod).forEach((key) => {
+    mod.add(ljp.mod, key, 0, 100);
+  });
+  mod.open()
+
+  const scale = gui.addFolder("Scale");
+  Object.keys(ljp.scale).forEach((key) => {
+    scale.add(ljp.scale, key, 0, 10);
+  });
+  scale.open()
+
+  const speed = gui.addFolder("Speed");
+  Object.keys(ljp.speed).forEach((key) => {
+    speed.add(ljp.speed, key, 0, 10);
+  });
+  speed.open()
+
 }
 
 function start (){
-
     initScene();
-
     initGUI();
 
     material = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors, linewidth : 5 } );
     geometry = new THREE.BufferGeometry;
 
-    var positions = new Float32Array( pointCount * 3);
-    var colors = new Float32Array( pointCount * 3 );
+    const positions = new Float32Array( pointCount * 3);
+    const colors = new Float32Array( pointCount * 3 );
 
     geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
     geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
 
     ljLine = new THREE.Line( geometry, material );
 
-    setRandomColors(ljLine);
+    setRandomColors(ljLine, pointCount);
 
     geometry.computeBoundingSphere();
 
@@ -86,22 +102,10 @@ function start (){
     camera.lookAt( new THREE.Vector3() );
 }
 
-function setRandomColors(line)
-{
-  var colors = line.geometry.attributes.color.array;
-  var index = 0;
-  for (var i = 0; i < pointCount; i ++ ) {
-    colors[ index ++ ] = Math.random();
-    colors[ index ++ ] = Math.random();
-    colors[ index ++ ] = Math.random();
-  }
-  line.geometry.attributes.color.needsUpdate = true; // VERY IMPORTANT!!
-}
-
 function animate() {
     requestAnimationFrame( animate );
-    var time = Date.now() * 0.1;
-    setLissajousPositions(ljLine, time, ljSettings, false);
+    ljp.offset = Date.now();
+    setLissajousPositions(ljLine, ljp, pointCount, false);
     // controls.update()
     renderer.render( scene, camera );
 };
