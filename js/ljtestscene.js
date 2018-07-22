e6deacc0a6e558f983fe7e8597b0a785b971ede3
Ryan Cashman <ryan.j.cashman@gmail.com>
2018-07-22 15:07:50 -0700

    removed stray classes, simplified testscene

--- js/ljtestscene.js
+++ js/ljtestscene.js
@@ -1,106 +1,101 @@
+// Lissajous Figures By Ryan Cashman
+// nanoanimal.com
 
-//THIS CouLD ALL BE WRAPPED UP INTO A MODULE?
+/*
 
-const sceneModule = ( function() {
-  //private
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
-  const renderer = new THREE.WebGLRenderer( { antialias : true } );
-  //public
-  return{
+globals THREE dat Float32Array lissajousModule
 
-    init : function(){
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      document.body.appendChild( renderer.domElement );
-      camera.position.z = 5;
-      camera.lookAt( new THREE.Vector3() );
-    },
+*/
 
-    add : function( geo )
-    {
-      scene.add( geo );
-    },
-
-    render : function(){
-      renderer.render( scene, camera );
-    }
-
-  };
-} )();
-
-const camMovement = 0;
-const pointCount = 2400;
-const gui = new dat.GUI();
-
-const ljParams = function()
-{
-  this.freq = [ 4, 8, 2 ],
-  this.mod = [ 1, 2, 3 ],
-  this.scale = [ 1, 1, 1 ],
-  this.speed = [ .1, .1, .1 ],
-  this.offset = 0
-}
-
+var camMovement = 0;
+var pointCount = 2400;
+var scene, camera, renderer, gui;
+var geometry, material, positions, colors;
 var ljp;
 var ljLine;
 
-function initGUI(){
-  ljp = new ljParams(); // for whatever reason this needs to be a new object.
+function lissajousParams(params)
+{
+  this.freq = params.freq || [ 14, 18, 12 ];
+  this.mod = params.mod || [ 5, 7, 9 ];
+  this.scale = params.scale || [ 1, 1, 1 ];
+  this.speed = params.speed || [ 0.1, 0.3, 0.4];
+  this.offset = params.offset = 0;
+}
 
-  const freq = gui.addFolder("Freq");
+function initScene(){
+  gui = new dat.GUI();
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100 );
+  renderer = new THREE.WebGLRenderer( { antialias : true } );
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+  camera.position.z = 3;
+  camera.lookAt( new THREE.Vector3() );
+
+  material = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors, linewidth : 5 } );
+  geometry = new THREE.BufferGeometry;
+  positions = new Float32Array( pointCount * 3);
+  colors = new Float32Array( pointCount * 3 );
+
+  ljp = new lissajousParams({});
+
+  geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+  geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+
+  ljLine = new THREE.Line( geometry, material );
+
+  lissajousModule.randomColors(ljLine, pointCount);
+
+  geometry.computeBoundingSphere();
+  scene.add( ljLine );
+}
+
+function initGUI(){
+
+  function setName(k){
+    return k==='0'?'x':k==='1'?'y':'z';
+  }
+
+  const freq = gui.addFolder("Frequency");
   Object.keys(ljp.freq).forEach((key) => {
-    freq.add(ljp.freq, key, 0, 100);
+    freq.add(ljp.freq, key, 0, 100).name(setName(key));
   });
   freq.open();
 
-  const mod = gui.addFolder("Mod");
+  const mod = gui.addFolder("Modulation");
   Object.keys(ljp.mod).forEach((key) => {
-    mod.add(ljp.mod, key, 0, 100);
+    mod.add(ljp.mod, key, 0, 100).name(setName(key));
   });
   mod.open()
 
   const scale = gui.addFolder("Scale");
   Object.keys(ljp.scale).forEach((key) => {
-    scale.add(ljp.scale, key, 0, 10);
+    scale.add(ljp.scale, key, 0, 10).name(setName(key));
   });
   scale.open()
 
   const speed = gui.addFolder("Speed");
   Object.keys(ljp.speed).forEach((key) => {
-    speed.add(ljp.speed, key, 0, 1);
+    speed.add(ljp.speed, key, -1, 1).name(setName(key));
   });
   speed.open()
 
 }
 
-function start (){
-    sceneModule.init();
+function init (){
+    initScene();
     initGUI();
-
-    const material = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors, linewidth : 5 } );
-    const geometry = new THREE.BufferGeometry;
-    const positions = new Float32Array( pointCount * 3);
-    const colors = new Float32Array( pointCount * 3 );
-
-    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-
-    ljLine = new THREE.Line( geometry, material );
-
-    lissajousModule.randomColors(ljLine, pointCount);
-    //const colorA = 0.2;
-    //const colorB = 0.6;
-    //lissajousModule.twoToneColors(ljLine, pointCount, colorA, colorB);
-
-    geometry.computeBoundingSphere();
-
-    sceneModule.add( ljLine );
-
 }
 
 function animate() {
     requestAnimationFrame( animate );
     ljp.offset = Date.now();
     lissajousModule.updatePoints(ljLine, ljp, pointCount, false);
-    sceneModule.render();
+    // sceneModule.render();
+    renderer.render( scene, camera );
 };
+
+init();
+animate();
